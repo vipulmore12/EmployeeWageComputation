e8da640f8f102645b0a3438aa782cf6063f08c29
vipulmore12 <vipulmore1998@gmail.com>
2020-12-14 18:16:19 +0530

    Part time and Daily Employee Wage

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,12 +2,15 @@ echo "Employee Wage Compuation Problem"
 isPresent=1
 wageRate=20
 dayHr=8
+partTimeHr=$(($RANDOM%8+1))
 randomCheck=$(( RANDOM%2 ))
 if [[ $isPresent -eq $randomCheck ]]
 then
-    echo "Employee is present"
+	echo "Employee is present"
 else
-     echo "Employee is not present"
+	echo "Employee is not present"
 fi
 dailyWage=$(( $wageRate * $dayHr ))
 echo "Daily employee wage = $dailyWage"
+partTimeWage=$(( $wageRate * $partTimeHr ))
+echo "For $partTimeHr part time hours wages are: $partTimeWage"
